766c1adaaffab91ed1e7a836eb094b2071aa273c
Emiaostein <cszwdy@gmail.com>
2016-04-11 13:31:12 +0800

    Update - Register Multi Fonts from URLs

---
+++ FontsManager/FontInformation.swift
@@ -0,0 +1,17 @@
+//
+//  FontInformation.swift
+//  FontsManager
+//
+//  Created by Emiaostein on 4/11/16.
+//  Copyright © 2016 Emiaostein. All rights reserved.
+//
+
+import Foundation
+
+struct FontInformation {
+    let fontFileName: String
+    let fontFileType: String
+    let fontFileSize: Int
+    let fontFullName: String
+    let fontFamily: String
+}
--- FontsManager/ViewController.swift
+++ FontsManager/ViewController.swift
@@ -20,23 +20,61 @@ class ViewController: UIViewController {
     }
     
     func setup() {
-        
+        registerFontsByURLs()
+    }
+    
+    func registerFont() {
         let fontFileName = "com.emiaostein.fonts.zaozigongfang.miaomiao"
         let path = NSBundle.mainBundle().pathForResource(fontFileName, ofType: "ttf")!
         let data = NSData(contentsOfFile: path)!
         let provider = CGDataProviderCreateWithCFData(data)
-        let cffont = CGFontCreateWithDataProvider(provider)
-        if let font = cffont where !CTFontManagerRegisterGraphicsFont(font, nil) {
-            print("Register Failture.")
+        let cgfont = CGFontCreateWithDataProvider(provider)
+        if let font = cgfont where CTFontManagerRegisterGraphicsFont(font, nil) {
+            let ctfont = CTFontCreateWithGraphicsFont(font, 1, nil, nil)
+            let familyName = CTFontCopyFamilyName(ctfont)
+            let fullName = CTFontCopyFullName(ctfont)
+            print("family: \(familyName) \nname: \(fullName) \nRegister Success.")
         } else {
-            print("Register Success.")
+            print("Register Failture.")
+        }
+    }
+    
+    func registerFontByURL() {
+        
+        let fontFileName = "com.emiaostein.fonts.zaozigongfang.miaomiao"
+        let path = NSBundle.mainBundle().pathForResource(fontFileName, ofType: "ttf")!
+        let url = NSURL(fileURLWithPath: path)
+        
+        if CTFontManagerRegisterFontsForURL(url, .None, nil) {
+            
+            print(UIFont.familyNames())
+        }
+    }
+    
+    func registerFontsByURLs() {
+
+        let fontsName = "Fonts"
+        let path = NSBundle.mainBundle().bundleURL
+        let url = path.URLByAppendingPathComponent(fontsName)
+        let fontUrls = try! NSFileManager.defaultManager().contentsOfDirectoryAtURL(url, includingPropertiesForKeys: nil, options: NSDirectoryEnumerationOptions.init(rawValue: 0))
+        
+        let urls = fontUrls.filter{ $0.pathExtension == "ttf" }
+        
+        let beforeFamilyName = UIFont.familyNames()
+        if CTFontManagerRegisterFontsForURLs(urls, .Process, nil) {
+            print(UIFont.familyNames().filter{ !beforeFamilyName.contains($0) })
         }
     }
     
     func fontChangedTo(name: String) {
         let descriptor = UIFontDescriptor(name: name, matrix: CGAffineTransformIdentity)
         let font = UIFont(descriptor: descriptor, size: 20)
-        
+        print(font.familyName)
+        textView.font = font
+    }
+    
+    func fontChangedBy(descriptor: UIFontDescriptor) {
+        let font = UIFont(descriptor: descriptor, size: 20)
         textView.font = font
     }
 }
@@ -47,5 +85,12 @@ extension ViewController {
         
         let fontName = "MF MiaoMiao (Noncommercial) Regular"
         fontChangedTo(fontName)
+        
+//        let fontFileName = "com.emiaostein.fonts.zaozigongfang.miaomiao"
+//        let path = NSBundle.mainBundle().pathForResource(fontFileName, ofType: "ttf")!
+//        let data = NSData(contentsOfFile: path)!
+//        if let descriptor = CTFontManagerCreateFontDescriptorFromData(data) {
+//            fontChangedBy(descriptor)
+//        }
     }
 }
